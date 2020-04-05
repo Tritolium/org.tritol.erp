58801d1473485aac58c276821f0a136546b9a01d
Tritolium <paracrafta@gmail.com>
2020-04-05 23:42:59 +0200

    Removed DataController

--- src/org/tritol/erp/controlling/Controller.java
+++ src/org/tritol/erp/controlling/Controller.java
@@ -2,32 +2,32 @@ package org.tritol.erp.controlling;
 
 import java.time.LocalDate;
 
-import org.tritol.erp.data.DataController;
+import org.tritol.erp.data.DataAccess;
 import org.tritol.erp.data.OrderState;
 
 public class Controller {
 
 	public static void addOrder(int order_nr, LocalDate order_date) {
-		DataController.getInstance().addOrder(order_nr, order_date);
+		DataAccess.addOrder(order_nr, order_date);
 	}
 
 	public static void addToOrder(int order_nr, String pos_nr, String art_desc, int quantity, double price) {
-		DataController.getInstance().addToOrder(order_nr, pos_nr, art_desc, quantity, price);
+		DataAccess.addToOrder(order_nr, pos_nr, art_desc, quantity, price);
 	}
 	
 	public static void setOrderState(int order_nr, OrderState state) {
-		DataController.getInstance().setOrderState(order_nr, state);
+		DataAccess.setOrderState(order_nr, state);
 		if(state == OrderState.ARRIVED) {
-			DataController.getInstance().setArrivalDate(order_nr, LocalDate.now());
-			DataController.getInstance().addOrderToStock(order_nr);
+			DataAccess.setArrivalDate(order_nr, LocalDate.now());
+			DataAccess.addOrderToStock(order_nr);
 		}
 	}
 	
 	public static void addConsumption(int con_nr, LocalDate con_date, String usage) {
-		DataController.getInstance().addConsumption(con_nr, con_date, usage);
+		DataAccess.addConsumption(con_nr, con_date, usage);
 	}
 	
-	public static void safeState() {
-		DataController.saveInstance();
+	public static void disconnect() {
+		DataAccess.disconnect();
 	}
 }
