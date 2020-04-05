5288840e87159b960ea0667af0ce9462cb5c14d4
Tritolium <paracrafta@gmail.com>
2020-04-05 23:50:44 +0200

    .

--- src/org/tritol/erp/application/ERP.java
+++ src/org/tritol/erp/application/ERP.java
@@ -8,7 +8,7 @@ import org.tritol.erp.data.OrderState;
 public class ERP {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 80; i++) {
+		for (int i = 0; i < 10; i++) {
 			Controller.addOrder(i, LocalDate.of(2020, 4, 3));
 			Controller.addToOrder(i, "0327", "Gärröhrchen ohne Dichtgummi", 1, 0.95d);
 			Controller.addToOrder(i, "0057-10", "Gummi Stopfen 24/18 mit 9mm Bohrung", 1, 1.80d);
