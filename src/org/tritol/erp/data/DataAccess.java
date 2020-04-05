c8ff8e1a9971092259c903d51036115aa4ed7016
Tritolium <paracrafta@gmail.com>
2020-04-05 18:11:21 +0200

    Added SQL-Connection and initialization of database

--- src/org/tritol/erp/data/DataAccess.java
+++ src/org/tritol/erp/data/DataAccess.java
@@ -1,8 +1,16 @@
 package org.tritol.erp.data;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DataAccess {
 	
@@ -12,8 +20,26 @@ public class DataAccess {
 	 * URL to the database
 	 */
 	private static final String database = "jdbc:sqlite:database.sqlite";
-	
+	/**
+	 * URL to serialized DataController instance
+	 */
+	private static final String ser_database ="./database.ser";
+
 	public static void connect() {
+		
+		File d_file = new File("database.sqlite");
+		
+		Boolean init = false;
+		
+		try {
+			if(!d_file.isFile()) {
+				d_file.createNewFile();								//create database file if there is none
+				init = true;
+			}								
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
 		try {
 			Class.forName("org.sqlite.JDBC");						//load the driver
 		} catch (ClassNotFoundException e) {
@@ -25,8 +51,60 @@ public class DataAccess {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
+		if(init) {
+			initDatabase();
+		}
 	}
 	
+	private static void initDatabase() {
+		try {
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(
+					"CREATE TABLE _order("
+					+ "order_nr int,"
+					+ "order_date date,"
+					+ "arrival_date date,"
+					+ "order_state varchar(1),"
+					+ "primary key (order_nr)"
+					+ ")");
+			statement.executeUpdate(
+					"CREATE TABLE consumption("
+					+ "con_nr int,"
+					+ "con_date date,"
+					+ "usage varchar(256),"
+					+ "primary key (con_nr)"
+					+ ")");
+			statement.executeUpdate(
+					"CREATE TABLE o_article("
+					+ "order_nr int,"
+					+ "pos_nr varchar(24),"
+					+ "art_desc varchar(256),"
+					+ "quantity int,"
+					+ "price real,"
+					+ "foreign key (order_nr) references _order (order_nr)"
+					+ ")");
+			statement.executeUpdate(
+					"CREATE TABLE c_article ("
+					+ "con_nr int,"
+					+ "pos_nr varchar(24),"
+					+ "art_desc varchar(256),"
+					+ "quantity int,"
+					+ "foreign key (con_nr) references consumption (con_nr)"
+					+ ")");
+			statement.executeUpdate(
+					"CREATE TABLE stock ("
+					+ "pos_nr varchar(24),"
+					+ "art_desc varchar(256),"
+					+ "quantity int,"
+					+ "price real,"
+					+ "primary key (pos_nr)"
+					+ ")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+	}
+
 	public static void disconnect() {
 		try {
 			if(connection != null) {
@@ -35,5 +113,39 @@ public class DataAccess {
 		} catch (SQLException e) {
 			System.err.println("Unable to disconnect");
 		}
+	}
+	
+	public static void serialize(DataController instance) {
+		FileOutputStream fos;
+		ObjectOutputStream oos;
+		try {
+			fos = new FileOutputStream(ser_database);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(instance);
+			oos.close();
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static DataController deserialize() {
+		FileInputStream fis;
+		ObjectInputStream ois;
+		DataController instance = null;
+		
+		try {
+			fis = new FileInputStream(ser_database);
+			ois = new ObjectInputStream(fis);
+			instance = (DataController) ois.readObject();
+			ois.close();
+			fis.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not load database");
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return instance;
 	}
 }
--- src/org/tritol/erp/data/DataController.java
+++ src/org/tritol/erp/data/DataController.java
@@ -1,10 +1,5 @@
 package org.tritol.erp.data;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -30,40 +25,19 @@ public class DataController implements Serializable{
 	
 	public static DataController getInstance() {
 		if(instance == null) {
-			//try to deserialize
-			FileInputStream fis;
-			ObjectInputStream ois;
-			try {
-				fis = new FileInputStream("./database.ser");
-				ois = new ObjectInputStream(fis);
-				instance = (DataController) ois.readObject();
-				ois.close();
-				fis.close();
-			} catch (IOException | ClassNotFoundException e) {
-				instance = new DataController();
-				System.err.println("failed to deserialize. Created new instance");
+			DataAccess.connect();
+			instance = DataAccess.deserialize();				//loading DataController
+			if(instance == null) {
+				instance = new DataController();				//creating new if loading was not successful
+				System.out.println("Created new database");
 			}
-			
-		
-			instance = new DataController();
 		}
 		return instance;
 	}
 	
 	public static void saveInstance() {
-		DataAccess.connect();
-		FileOutputStream fos;
-		ObjectOutputStream oos;
-		try {
-			fos = new FileOutputStream("./database.ser");
-			oos = new ObjectOutputStream(fos);
-			oos.writeObject(getInstance());
-			oos.close();
-			fos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		DataAccess.disconnect();
+		DataAccess.serialize(getInstance());
 	}
 
 	public void addOrder(int order_nr, LocalDate order_date) {
